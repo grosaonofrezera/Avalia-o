f67de36fcd4bac4e412443f1b474ab0746049a55
GabrielRosa <g.rosaonofre@gmail.com>
2026-08-28 08:50:47 -0300

    primeira parte da classe dinossauro feita!!

---
+++ Dinossauros.cs
@@ -0,0 +1,28 @@
+public class Dinossauro
+
+{
+    private string? nome;
+
+    private string? periodo;
+
+    public string? Nome
+    {
+        get {return nome;}
+        set {nome = value;}
+    }
+
+    public string? Periodo
+    {
+        get {return periodo;}
+        set {periodo = value;}
+    }
+
+    public Dinossauro(string nome, string periodo)
+    {
+        this.nome = nome;
+        this.periodo = periodo;
+ 
+    }
+
+    
+}
