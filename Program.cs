dcfc32bceca0f9101f54539901bfef3a412c9c20
GabrielRosa <g.rosaonofre@gmail.com>
2026-08-31 17:06:07 -0300

    uns bglh aleatorio mas Dinossauro finalizado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿Dinossauro dinossauro1 = new Dinossauro("T-Rex", "Cretáceo")
-Dinossauro dinossauro2 = new Dinossauro("Giganotossauro", "Cretáceo")
+﻿Dinossauro dinossauro1 = new Dinossauro("T-Rex", "Cretáceo");
+Dinossauro dinossauro2 = new Dinossauro("Giganotossauro", "Cretáceo");
 
+Console.WriteLine("   🦖Dinossauros🦖   ");
 Console.WriteLine(dinossauro1);
+Console.WriteLine("----------------");
 Console.WriteLine(dinossauro2);
